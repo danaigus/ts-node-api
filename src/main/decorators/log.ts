a5bdbd812ab7a98a2110ddf79232ba64c846bb0f
José Marcos Lopes <josemarcosld@gmail.com>
2022-04-04 15:33:34 -0500

    feat: ensure LogControllerDecorator calls controller with correct value

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,19 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+
+export class LogControllerDecorator implements Controller {
+  private readonly controller: Controller
+
+  constructor (controller: Controller) {
+    this.controller = controller
+  }
+
+  async handle (httpResquest: HttpRequest): Promise<HttpResponse> {
+    await this.controller.handle(httpResquest)
+    return null
+    // const httpResponse = await this.controller.handle(httpResquest)
+    // if (httpResponse.statusCode === 500) {
+
+    // }
+    // return httpResponse
+  }
+}
