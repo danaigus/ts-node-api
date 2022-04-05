802b4493a1d18dedb383cebc110b8c812d07c12c
José Marcos Lopes <josemarcosld@gmail.com>
2022-04-05 17:43:53 -0500

    feat: ensure ExpressRouteAdapter returns the error message instead of the error name

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -7,7 +7,6 @@ export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
-    // const account = { ...accountData, id: result.insertedId.toString() }
     const account = await accountCollection.findOne(result.insertedId)
     return MongoHelper.map(account)
   }
